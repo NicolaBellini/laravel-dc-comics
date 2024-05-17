9799dc3e114a6da8d05d80443e38ac08a6590159
NicolaBellini <bellininicola4366@gmail.com>
2024-05-17 14:19:54 +0200

    [fix] comics columns

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -6,12 +6,15 @@
   <div class="palette d-flex ">
     @foreach ($comics as  $comic)
         <div  class="color d-flex flex-column">
-                <h5>{{$comic->title}}</h5>
-                <a class="" href="{{route('comics.show',$comic)}}">Dettagli</a>
-                <a class="" href="{{route('comics.edit',$comic)}}">modifica</a>
+            <div class="text">
+                <h5 class="text-wrap">{{$comic->title}}</h5>
+                <a  href="{{route('comics.show',$comic)}}">Dettagli</a>
+                <a  href="{{route('comics.edit',$comic)}}">modifica</a>
                 {{-- <a class="" href="{{route('comics.edit',$comic)}}">modifica</a> --}}
                 {{-- delete --}}
                 <form action=""></form>
+
+            </div>
 
         </div>
         @endforeach
