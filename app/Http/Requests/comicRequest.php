68ad3c3113e6ea5435cf2c392cd6a4ead8f17ef7
NicolaBellini <bellininicola4366@gmail.com>
2024-05-20 13:39:13 +0200

    [add] rules and messages

--- app/Http/Requests/comicRequest.php
+++ app/Http/Requests/comicRequest.php
@@ -22,7 +22,28 @@ class comicRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title'=>'required'|'min:1'|'max:100',
+            'description'=>'nullable',
+            'price'=>'max:20'|'required',
+            'series'=>'max:100'|'required',
+            'sale_date'=>'nullable',
+            'type'=>'max:100'|'required',
+            'img'=>'nullable'|'url',
+            'artist'=>'nullable',
+            'writers'=>'nullable',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Il titolo è un campo obbligatorio',
+            'price.required' => 'Il prezzo è un campo obbligatorio',
+            'price.max' => 'Il prezzo non può superare 20 caratteri',
+            'series.required' => 'La serie è un campo obbligatorio',
+            'type.required' => 'Il tipo è un campo obbligatorio',
+            'sale_date.date' => 'La data di vendita deve essere una data valida',
+            'img.url'=>'l\' url immessa non è valida'
         ];
     }
 }
