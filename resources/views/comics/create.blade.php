89fb6cd83604432126342e590dd13037251313d6
NicolaBellini <bellininicola4366@gmail.com>
2024-05-20 14:00:07 +0200

    [ad] error message and eror class in input form title

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
 
+@dump($errors->all())
+
 <h1 class="text-center">INSERISCI UN NUOVO FUMETTO</h1>
 <form action="{{ route('comics.store') }}" method="POST" class="form-control">
     @csrf
@@ -9,50 +11,57 @@
     <div class="row mb-3">
         <div class="col">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="Inserisci il titolo" >
+            <input type="text" name="title" class="form-control @error('title') is-invalid  @enderror" id="title" placeholder="Inserisci il titolo" value="{{old('title')}}">
+            @error('title')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+
+
         </div>
 
         <div class="col">
             <label for="price" class="form-label">Prezzo</label>
-            <input type="text" name="price" class="form-control" id="price" placeholder="Inserisci il prezzo" >
+            <input type="text" name="price" class="form-control" id="price" placeholder="Inserisci il prezzo" value="{{old('price')}}">
         </div>
     </div>
 
     <div class="mb-3">
         <label for="description" class="form-label">Descrizione</label>
-        <textarea class="form-control" name="description" id="description" rows="3" placeholder="Inserisci la descrizione" ></textarea>
+        <textarea class="form-control" name="description" id="description" rows="3" placeholder="Inserisci la descrizione" value="{{old('description')}}" ></textarea>
     </div>
 
     <div class="row mb-3">
         <div class="col">
             <label for="series" class="form-label">Serie</label>
-            <input type="text" name="series" class="form-control" id="series" placeholder="Inserisci la serie" >
+            <input type="text" name="series" class="form-control" id="series" placeholder="Inserisci la serie" value="{{old('series')}}">
         </div>
 
          <div class="col">
             <label for="series" class="form-label">immagine</label>
-            <input type="text" name="img" class="form-control" id="series" placeholder="Inserisci l' url dell' immagine" >
+            <input type="text" name="img" class="form-control" id="series" placeholder="Inserisci l' url dell' immagine" value="{{old('img')}}">
         </div>
 
         <div class="col">
             <label for="sale_date" class="form-label">Data di vendita</label>
-            <input type="date" name="sale_date" class="form-control" id="sale_date">
+            <input type="date" name="sale_date" class="form-control" id="sale_date" value="{{old('sale_date')}}">
         </div>
     </div>
 
     <div class="mb-3">
         <label for="type" class="form-label">Tipo</label>
-        <input type="text" name="type" class="form-control" id="type" placeholder="Inserisci il tipo" >
+        <input type="text" name="type" class="form-control" id="type" placeholder="Inserisci il tipo" value="{{old('type')}}">
     </div>
 
     <div class="mb-3">
         <label for="artists" class="form-label">Artisti</label>
-        <input type="text" name="artists" class="form-control" id="artists" placeholder="Inserisci gli artisti" >
+        <input type="text" name="artists" class="form-control" id="artists" placeholder="Inserisci gli artisti" value="{{old('artists')}}">
     </div>
 
     <div class="mb-3">
         <label for="writers" class="form-label">Scrittori</label>
-        <input type="text" name="writers" class="form-control" id="writers" placeholder="Inserisci gli scrittori" >
+        <input type="text" name="writers" class="form-control" id="writers" placeholder="Inserisci gli scrittori" value="{{old('writers')}}">
     </div>
 
     <button type="submit" class="btn btn-primary">Invia</button>
