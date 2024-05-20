8182afdbcd2c5f1b6f8368b7cb7d338f1e793c12
NicolaBellini <bellininicola4366@gmail.com>
2024-05-20 15:00:45 +0200

    [add] home link in logo header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
   <header class="dc-header">
     <div class="container">
-      <h1 class="logo">BOOLOMICS</h1>
+      <a class="logo text-white text-decoration-none" href="{{route('home')}}">BOOLOMICS</a>
       <nav class="nav-links">
         <a href="{{route('home')}}">Home</a>
         <a href="{{route('comics.create')}}">Crea un Fumetto</a>
